ec153e60e1d2781eae271f2d45b0459bfc88b167
eugeniosolucoes <webmaster@eugeniosolucoes.com.br>
2017-08-10 21:01:49 -0300

    Incluindo saudação!

--- app/src/main/java/br/com/eugeniosolucoes/cfm3/MainActivity.java
+++ app/src/main/java/br/com/eugeniosolucoes/cfm3/MainActivity.java
@@ -2,9 +2,8 @@ package br.com.eugeniosolucoes.cfm3;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        initCFM();
     }
 
     @Override
@@ -97,5 +99,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void initCFM(){
+        TextView txt = (TextView) findViewById(R.id.txtBemVindo);
+        txt.setText("Bem-vindo");
     }
 }
